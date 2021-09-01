7c9c4a47567aedb1d93627129a6db2301ec1ec8c
Milan Skocic <milan.skocic@gmail.com>
2021-09-01 16:47:52 +0200

    ENH: Added minimize

--- optimization/optimization.py
+++ optimization/optimization.py
@@ -18,10 +18,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Author(s): Milan Skocic <milan.skocic@gmail.com>
 """
+import time
 from tkinter.constants import W
 from typing import Callable
 import numpy as np
-from numpy.core.numeric import full
 from numpy.typing import ArrayLike
 from scipy import optimize
 
@@ -97,7 +97,7 @@ def chi2(p: ArrayLike,
     res: ArrayLike
         Algebric residuals.
     """
-    res = np.sum(np.absolute(residuals(p, x, y, w, model)))
+    res = np.sum(np.absolute(residuals(p, x, y, w, model))**2)
     return res
 
 def lm_func(p: ArrayLike, 
@@ -157,25 +157,47 @@ def nm_func(p: ArrayLike,
     res = chi2(p, x, y, w, model)
     return res
 
-x = np.linspace(0, 10, 10)
+x = np.linspace(0, 10, 10000)
 p0 = np.asarray((1.1, 2.3))
 y = model(p0, x)
 w = np.ones_like(y)
 
 p0 = np.asarray((10, 10))
+t0 = time.time()
 popt, cov, infodict, ier, msg = optimize.leastsq(lm_func, p0, args=(x, y, w, model), full_output=True)
-print('optimize.leastsq: ', popt)
+t1 = time.time()
+t = (t1-t0)*1e3
+print('optimize.leastsq: ', popt, t)
 
 
 p0 = np.asarray((10, 10))
+t0 = time.time()
 popt = optimize.fmin(nm_func, p0, args=(x, y, w, model), disp=0)
-print('optimize.fmin: ', popt)
+t1 = time.time()
+t = (t1-t0)*1e3
+print('optimize.fmin: ', popt, t)
 
 
 p0 = np.asarray((10, 10))
+t0 = time.time()
 res = optimize.least_squares(lm_func, p0, args=(x, y, w, model))
-print('optimize.least_square (trf): ', res.x)
+t1 = time.time()
+t = (t1-t0)*1e3
+print('optimize.least_square (trf): ', res.x, t)
 
 p0 = np.asarray((10, 10))
+t0 = time.time()
 res = optimize.least_squares(lm_func, p0, args=(x, y, w, model), method='lm')
-print('optimize.least_square (lm): ', res.x)
+t1 = time.time()
+t = (t1-t0)*1e3
+print('optimize.least_square (lm): ', res.x, t)
+print(chi2(res.x, x, y, w, model), res.cost*2)
+
+
+p0 = np.asarray((10, 10))
+t0 = time.time()
+res = optimize.minimize(nm_func, p0, args=(x, y, w, model), method='nelder-mead')
+t1 = time.time()
+t = (t1-t0)*1e3
+print('optimize.minimize (nerlder): ', res.x, t)
+print(chi2(res.x, x, y, w, model), res.fun)
