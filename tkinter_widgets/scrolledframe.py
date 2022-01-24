70117e501a9478ffe32e67eb5ca90b54d2dc90e7
Milan Skocic <milan.skocic@gmail.com>
2022-01-24 09:09:38 +0100

    ENH: Add Tk widgets examples.

--- tkinter_widgets/scrolledframe.py
+++ tkinter_widgets/scrolledframe.py
@@ -1,12 +1,29 @@
 r"""
-Scrolled custom widgets.
+Copyright (C) 2020-2021 Milan Skocic.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Author: Milan Skocic <milan.skocic@gmail.com>
 """
-from tkinter import ttk
 import tkinter as tk
+from tkinter import ttk
+
+
 class ScrolledFrame(ttk.Frame):
     r"""Class for scrolled frames. See __init__.__doc__."""
 
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, scrolled:str="both", **kwargs):
         r"""
         Scrolled Frame widget which may contain other widgets and can have a 3D border.
 
@@ -14,17 +31,18 @@ class ScrolledFrame(ttk.Frame):
         ------------
         master: tkinter widget
             Master container.
+        scrolled: str
+            Indicates the scroll bars. Can be x, y, xy or both.
         kwargs: dict, optional
-            Keyword arguments for the scrolled frame.
-        """
-        ttk.Frame.__init__(self, master)
-        self._default_options = {'scrolled': 'y'}
-        self.pack(expand=tk.TRUE, fill=tk.BOTH)
-        for i in kwargs:
-            if i not in self._default_options.keys():
-                raise tk.TclError('Unknow option --' + i)
+            Keyword arguments for the master Frame
 
-        self._default_options.update(kwargs)
+        """
+        ttk.Frame.__init__(self, master, **kwargs)
+        self._default_options = ("x", "y", "xy", "both") 
+        self.pack(expand=tk.TRUE, fill=tk.BOTH)
+        if scrolled not in self._default_options:
+            raise ValueError(f"scrolled can be x, y, xy or both.")
+
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=0)
@@ -34,17 +52,15 @@ class ScrolledFrame(ttk.Frame):
         self.yscrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
         self.xscrollbar = ttk.Scrollbar(self, orient=tk.HORIZONTAL)
 
-        if self._default_options['scrolled'] == 'y':
+        if scrolled == 'y':
             self.yscrollbar.grid(row=0, column=1, sticky='ns')
-        elif self._default_options['scrolled'] == 'x':
+        elif scrolled == 'x':
             self.xscrollbar.grid(row=1, column=0, sticky='ew')
-        elif self._default_options['scrolled'] == 'both':
+        elif (scrolled == 'both') or (scrolled == "xy"):
             self.yscrollbar.grid(row=0, column=1, sticky='ns')
             self.xscrollbar.grid(row=1, column=0, sticky='ew')
         else:
-            raise tk.TclError('Bad scroll style \"' + 
-                              self._default_options['scrolled'] + 
-                              '\" must be x, y or both')
+            raise ValueError(f"scrolled can be x, y, xy or both.")
 
         self._canvas = tk.Canvas(self, bd=0, relief=tk.FLAT, 
                                  yscrollcommand=self.yscrollbar.set,
